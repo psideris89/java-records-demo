b53032a704232bac23390204331ee9c63bf61205
psideris <psideris1989@gmail.com>
2020-04-17 22:07:09 +0100

    Add custom constructor in Director Record

--- src/main/java/com/psideris/moviesapi/records/Director.java
+++ src/main/java/com/psideris/moviesapi/records/Director.java
@@ -2,5 +2,10 @@ package com.psideris.moviesapi.records;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Director(@JsonProperty("name") String name, @JsonProperty("surname") String surname) {
+public record Director(@JsonProperty("name")String name, @JsonProperty("surname")String surname) {
+
+    public Director {
+        if (surname == null)
+            throw new IllegalArgumentException("Surname required");
+    }
 }
